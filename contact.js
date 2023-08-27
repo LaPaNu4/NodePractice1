631a82f4b69b7bfd0651799f6b71cb3e8c2130c7
LaPaNu4 <120036931+LaPaNu4@users.noreply.github.com>
2023-08-27 23:15:59 +0300

    st

---
+++ contact.js
@@ -0,0 +1,20 @@
+import fs from "fs/promises";
+import path from "path";
+
+const contactsPath = path.resolve("db", "contacts.json");
+
+function listContacts() {
+  // ...твій код. Повертає масив контактів.
+}
+
+function getContactById(contactId) {
+  // ...твій код. Повертає об'єкт контакту з таким id. Повертає null, якщо контакт з таким id не знайдений.
+}
+
+function removeContact(contactId) {
+  // ...твій код. Повертає об'єкт видаленого контакту. Повертає null, якщо контакт з таким id не знайдений.
+}
+
+function addContact(name, email, phone) {
+  // ...твій код. Повертає об'єкт доданого контакту.
+}
